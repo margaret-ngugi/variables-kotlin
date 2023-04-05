fcc1b8ae46d15d76165ceb56b592b7062d674a4b
margaret-ngugi <ngugiwairimumargaret@gmail.com>
2023-04-05 08:19:38 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main() {
+school()
+var name = "Cynthia"
+println(name)
+}
+
+fun school(){
+    val school = "akirachix"
+    println(school[0])
+    println(school[2])
+    println(school[3])
+    println("a,i,r")
+}
+fun name(){
+
+}
